6263b937d6cea161e9c52663729ae73fe4cf8fc0
sobhangi <shobhangisarkar@gmail.com>
2019-01-03 00:15:47 +0530

    secondary commit
    changes

---
+++ code.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing dataset
+dataset = pd.read_csv('DATASET PROJECT.csv')
+X= dataset.iloc[:,0:5].values
+Y=dataset.iloc[:,5].values
